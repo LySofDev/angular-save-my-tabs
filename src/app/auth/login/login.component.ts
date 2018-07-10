02bbda2c1eb8cbc30a2e7553242bc7d48d3b223e
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-07-10 18:24:05 -0400

    Added login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+    private flash: MatSnackBar
+  ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      email: ['', [
+        Validators.required
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8)
+      ]]
+    });
+  }
+
+  onSubmit() {
+    this.auth.authenticateWith({
+      email: this.form.get('email').value,
+      password: this.form.get('password').value
+    }).subscribe((success: boolean) => {
+      if (success) {
+        const email = this.auth.userAsPrincipal().email;
+        this.flash.open(`Signed in successfully as ${email}.`, 'Ok');
+        this.router.navigate(['/tabs']);
+      } else {
+        this.flash.open('Invalid email or password', 'Ok');
+        this.clearForm();
+      }
+    });
+  }
+
+  clearForm() {
+    Object.keys(this.form.controls).forEach((field: string) => {
+      this.form.controls[field].setValue('');
+    });
   }
 
 }
